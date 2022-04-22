794788b22819ed51033d551e98f3248675c9aed6
Tanya <krajnikova2004@gmail.com>
2022-04-22 23:40:41 +0300

    Update validation: add constants for messages

--- src/utils/Validator.java
+++ src/utils/Validator.java
@@ -9,7 +9,7 @@ public class Validator {
     public static String validateName(Scanner scanner) {
         String str = scanner.nextLine().trim();
         while (str.isEmpty()) {
-            System.out.print("Empty value! Enter name: ");
+            System.out.print(Constants.EMPTY_NAME_MSG);
             str = scanner.nextLine().trim();
         }
         return str;
@@ -19,14 +19,14 @@ public class Validator {
     public static int validateQuantityInput(Scanner scanner) {
         while (!scanner.hasNextInt()) {
             String str = scanner.nextLine().trim();
-            System.out.printf("\"%s\" - not a number!%nEnter quantity: ", str);
+            System.out.printf(Constants.NAN_QUANTITY_MSG, str);
         }
         int quantity = scanner.nextInt();
         while (quantity <= 0) {
-            System.out.println("Incorrect value! Enter quantity: ");
+            System.out.println(Constants.INCORRECT_QUANTITY_MSG);
             while (!scanner.hasNextInt()) {
                 String str = scanner.next().trim();
-                System.out.printf("\"%s\" - not a number!%nEnter quantity: ", str);
+                System.out.printf(Constants.NAN_QUANTITY_MSG, str);
             }
             quantity = scanner.nextInt();
         }
@@ -37,14 +37,14 @@ public class Validator {
     public static double validatePriceInput(Scanner scanner) {
         while (!scanner.hasNextDouble()) {
             String str = scanner.nextLine().trim();
-            System.out.printf("\"%s\" - not a number!%nEnter price: ", str);
+            System.out.printf(Constants.NAN_PRICE_MSG, str);
         }
         double price = scanner.nextDouble();
         while (price <= 0) {
-            System.out.print("Incorrect value! Enter price: ");
+            System.out.print(Constants.INCORRECT_PRICE_MSG);
             while (!scanner.hasNextDouble()) {
                 String str = scanner.nextLine().trim();
-                System.out.printf("\"%s\" - not a number!%nEnter price: ", str);
+                System.out.printf(Constants.NAN_PRICE_MSG, str);
             }
             price = scanner.nextDouble();
         }

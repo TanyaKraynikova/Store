c85046170fc11f817da4daaea12003a59cee7b23
Tanya <krajnikova2004@gmail.com>
2022-04-22 22:57:21 +0300

    Create method validate data

--- src/views/PurchaseView.java
+++ src/views/PurchaseView.java
@@ -1,6 +1,9 @@
 package views;
 
 import models.Purchase;
+import utils.Validator;
+
+import java.util.Scanner;
 
 // View
 public class PurchaseView {
@@ -10,10 +13,43 @@ public class PurchaseView {
     String name;
     int quantity;
     double price;
+    Scanner scanner;
 
     // Конструктор
     public PurchaseView(Purchase model) {
 
         this.model = model;
+    }
+
+    // Ввод данных
+    public void getInputs() {
+
+        // Создаем экземпляр Scanner
+        scanner = new Scanner(System.in);
+
+        // Ввод и валидация данных
+
+        title = "Client name: ";
+        System.out.print(title);
+        name = Validator.validateName(scanner);
+        model.setClientName(name);
+
+        title = "Quantity of goods (pieces): ";
+        System.out.print(title);
+        quantity = Validator.validateQuantityInput(scanner);
+        model.setQuantity(quantity);
+
+        title = "Price of the goods piece (USD): ";
+        System.out.print(title);
+        price = Validator.validatePriceInput(scanner);
+        model.setPrice(price);
+
+        // Закрываем Scanner
+        scanner.close();
+    }
+
+    // Вывод данных
+    public void getOutput(String output) {
+        System.out.println(output);
     }
 }

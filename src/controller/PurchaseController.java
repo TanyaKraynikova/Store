736d5b0bab701e673abcba05a5521604bfb5ddcf
Tanya <krajnikova2004@gmail.com>
2022-04-22 23:19:36 +0300

    Add controller for purchase

---
+++ src/controller/PurchaseController.java
@@ -0,0 +1,16 @@
+package controller;
+
+import models.Purchase;
+import views.PurchaseView;
+
+public class PurchaseController {
+
+    Purchase model;
+    PurchaseView view;
+
+    // Конструктор
+    public PurchaseController(Purchase model, PurchaseView view) {
+        this.model = model;
+        this.view = view;
+    }
+}

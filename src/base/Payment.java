d833390f0aae53c3181dde24f141494750a86690
Tanya <krajnikova2004@gmail.com>
2022-04-22 21:49:27 +0300

    Add interface for payment

---
+++ src/base/Payment.java
@@ -0,0 +1,10 @@
+package base;
+
+// Интерфейс платежа
+public interface Payment {
+
+    double calculatePayment(int quota, double price);
+
+    double calculatePayment(double payment, double discount);
+}
+
